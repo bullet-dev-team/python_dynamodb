e0737ff1de07473bb1f94e2047d8a93ee45062b6
Lihang He <kxyjm.911@gmail.com>
2020-11-15 18:52:27 -0800

    init commit

---
+++ application.py
@@ -0,0 +1,17 @@
+from .MovieTable import create_movie_table
+import os
+import json
+
+def application(environ, start_response):
+  table = create_movie_table()
+  response = {
+    'envVar': os.getenv('NAME'),
+    'tableStatus': table.table_status
+  }
+  start_response("200 OK", [
+    ("Content-Type", "application/json")
+  ])
+  return [json.dumps(response).encode('utf-8')]
+
+
+
