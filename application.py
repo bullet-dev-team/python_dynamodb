d4a976030f0221dd46fe36b7a81a157bb6667a58
Lihang He <kxyjm.911@gmail.com>
2020-11-15 21:48:19 -0800

    Update application.py

--- application.py
+++ application.py
@@ -1,17 +1,40 @@
 from package.MovieTable import create_movie_table
-import os
 import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
 
 def application(environ, start_response):
-  table = create_movie_table()
+  client = boto3.client('dynamodb')
+  dynamodb = boto3.resource('dynamodb')
+  tableName = 'Movies'
+  table = dynamodb.Table(tableName)
+  existing_tables = client.list_tables()['TableNames']
+  if tableName not in existing_tables:
+    table = create_movie_table(tableName)
+
+  table.put_item(
+      Item={
+        'year': 2020,
+        'title': 'new Movie',
+        'info': {
+          'plot': 'charpter 1',
+          'rating': 10
+        }
+      }
+  )
+
+  item = table.query(
+      KeyConditionExpression=Key('year').eq(2020)
+  )
+
   response = {
-    'envVar': os.getenv('NAME'),
-    'tableStatus': table.table_status
+    'tableStatus': table.table_status,
+    'movie': str(item['Items'][0])
   }
+
   start_response("200 OK", [
     ("Content-Type", "application/json")
   ])
+
   return [json.dumps(response).encode('utf-8')]
-
-
-
